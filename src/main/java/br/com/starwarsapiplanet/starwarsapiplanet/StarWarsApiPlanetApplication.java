043789de87f8e0d1095fa36761dca0cfaf612342
murilo chagas <murilochagas10@outlook.com>
2023-05-17 15:30:18 -0300

    criacao do projeto

---
+++ src/main/java/br/com/starwarsapiplanet/starwarsapiplanet/StarWarsApiPlanetApplication.java
@@ -0,0 +1,13 @@
+package br.com.starwarsapiplanet.starwarsapiplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsApiPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsApiPlanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/starwarsapiplanet/starwarsapiplanet/StarWarsApiPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.starwarsapiplanet.starwarsapiplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsApiPlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
